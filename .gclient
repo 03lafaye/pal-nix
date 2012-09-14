4d72c076aec8cd27357d7c1cc031d0d99f2befc6
Pierre-Antoine LaFayette <plafayet@codeaurora.org>
2012-09-14 16:02:28 -0400

    Adding chrome for android build script and .gclient file

--- .gclient
+++ .gclient
@@ -1,6 +1,6 @@
 solutions = [
   { "name"        : "src",
-    "url"         : "http://src.chromium.org/chrome/trunk/src.git",
+    "url"         : "http://git.chromium.org/chromium/src.git",
     "deps_file"   : ".DEPS.git",
     "managed"     : True,
     "custom_deps" : {
@@ -9,9 +9,10 @@ solutions = [
         "src/chrome/tools/test/reference_build/chrome": None,
         "src/chrome_frame/tools/test/reference_build/chrome": None,
         "src/chrome/tools/test/reference_build/chrome_linux": None,
-        "src/chrome/tools/test/reference_build/chrome_mac": None,
+        "src/chrome/tools/test/reference_build/chrome_mac": None, 
     },
     "safesync_url": "",
   },
 ]
+
 target_os = ["android"]
---
+++ scripts/build-chrome.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+# Helper for building Chrome for Android
+
+echo "Looking for gclient..."
+
+if ! command -v gclient
+then
+    echo "gclient not found... make sure depot_tools is in your PATH"
+    exit 1
+fi
+
+if [ ! -d src ]
+then
+    echo "No src directory found... configuring gclient"
+    gclient config http://git.chromium.org/chromium/src.git --git-deps
+
+    if [ -f $HOME/pal-nix/.gclient ]
+    then
+        cp $HOME/pal-nix/.gclient .gclient
+    fi
+fi
+
+echo "Executing gclient sync..."
+
+time gclient sync -v --nohooks
+
+if [ ! -d src/build/android ]
+then
+    echo "Could not find src/build/android directory... gclient sync failed"
+    exit 1
+fi
+
+echo "Setting up Chrome for Android build environment..."
+pushd src
+. build/android/envsetup.sh
+
+echo "Executing gclient runhooks..."
+gclient runhooks
+
+# You may need to install the build dependencies for android:
+# pushd build
+# sudo ./install-build-deps-android.sh
+# popd
+
+echo "Executing android_gyp..."
+android_gyp
+
+echo "Building Chrome for Android..."
+time make -j12
