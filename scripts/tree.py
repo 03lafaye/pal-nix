38980fd51d5f1f976b1e8153bfdc029ece50bb91
Pierre-Antoine LaFayette <pierre.lafayette@gmail.com>
2014-08-19 10:46:41 -0400

    Add script to output directory tree

---
+++ scripts/tree.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+
+import os
+import pdb
+import time
+import sys
+
+EXCLUDED_EXTENSIONS = ['.swp', '.swo', '.swn', '.swm', '.pyc', '.class']
+EXCLUDED_FILES = ['.git', '.repo', 'out', '.svn', '.DS_Store', '.gitignore']
+
+PREFIX_CHARS = '+-- ';
+BRANCH_CHARS = '|';
+
+def print_tree(root, depth=0):
+    if depth == 0:
+        prefix = PREFIX_CHARS
+    else:
+        prefix = '%s%s%s' % (BRANCH_CHARS, ' ' * depth * 4, PREFIX_CHARS)
+
+    basename = os.path.basename(root) 
+    name, extension = os.path.splitext(basename)
+
+    if name not in EXCLUDED_FILES and extension not in EXCLUDED_EXTENSIONS: 
+        print('%s%s' % (prefix, basename))
+
+    if not os.path.isdir(root):
+        return
+
+    for filename in os.listdir(root):
+        print_tree(filename, depth + 1)
+
+
+if __name__ == '__main__':
+    print_tree(os.path.abspath('.'))
